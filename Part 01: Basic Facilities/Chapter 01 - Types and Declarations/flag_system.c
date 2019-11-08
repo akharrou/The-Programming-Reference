fadc54c85605e6a8a61f0761aea489aa92be134c
Aymen Kh <idev.aymen@gmail.com>
2019-11-08 13:27:04 -0800

    Update flag_system.c

--- Part 01: Basic Facilities/Chapter 01 - Types and Declarations/flag_system.c
+++ Part 01: Basic Facilities/Chapter 01 - Types and Declarations/flag_system.c
@@ -209,7 +209,7 @@ Bitset: 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
 
 /*
  * Compiled with: clang -std=c11
- * Files: flag_system.c readbits.c
+ * Files: ./flag_system.c ./readbits.c
  *
  * Compiler:
  * Apple LLVM version 9.1.0 (clang-902.0.39.1)
